38d24d4e21a408a3cc5f65b58afebc2fe8ad7d09
LORDyyyyy <mlord6271@gmail.com>
2023-11-04 16:25:07 +0200

    3 API status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-	const api = 'http://0.0.0.0:5001/api/v1/status/';
+	let amenitiesData = {}
 
-	$.getJSON(api, function(data) {
+        $('.amenities input[type=checkbox]').on('change', function() {
+                const amenityId = this.dataset.id;
+                const amenityName = this.dataset.name;
+                if (this.checked) {
+                        amenitiesData[amenityId] = amenityName;
+                } else {
+                        delete amenitiesData[amenityId];
+                }
+                const amenitiesList = Object.values(amenitiesData).join(', ');
+                $(".amenities h4").text(amenitiesList);
+        });
+
+	const api = 'http://0.0.0.0:5001/api/v1/status/';
+	console.log("Res:")
+	$.getJSON(api, (data) => {
 		if (data.status === 'OK') {
 			$('#api_status').addClass('available');
 			console.log("YES!!!");
