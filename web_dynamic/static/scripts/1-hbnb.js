fabc366c7651787c9fdbbb4e5c154798730adc82
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-11-05 23:12:19 +0200

    4. Fetch places

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-	let amenitiesData = {}
+$(document).ready(function () {
+  let amenitiesData = {};
 
-	$('.amenities input[type=checkbox]').on('change', function() {
-		const amenityId = this.dataset.id;
-	        const amenityName = this.dataset.name;
-		if (this.checked) {
-			amenitiesData[amenityId] = amenityName;
-	        } else {
-        		delete amenitiesData[amenityId];
-        	}
-		const amenitiesList = Object.values(amenitiesData).join(', ');
-	        $(".amenities h4").text(amenitiesList);
-	});
+  $('.amenities input[type=checkbox]').on('change', function () {
+    const amenityId = this.dataset.id;
+    const amenityName = this.dataset.name;
+    if (this.checked) {
+      amenitiesData[amenityId] = amenityName;
+    } else {
+      delete amenitiesData[amenityId];
+    }
+    const amenitiesList = Object.values(amenitiesData).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
 });
